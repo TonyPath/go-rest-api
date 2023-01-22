c4ba178413c6187b87ad05efbfdd9ed5c5738222
Antonis Papatheodorou <antonisp@comino-systems.com>
2023-01-22 13:42:19 +0200

    add health check endpoint

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/TonyPath/go-rest-api/internal/transport/http"
+)
 
 // App contains all the system depedencies like database connection, etc.
 type App struct {
@@ -8,11 +13,20 @@ type App struct {
 
 func (app *App) Run() error {
 	fmt.Println("Setting up out APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
 	return nil
 }
 
 func main() {
-	fmt.Print("hello")
+	fmt.Println("Go REST API")
 	app := App{}
 	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up out REST API")
