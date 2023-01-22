e430e57cdebc679b47217585d303194e2fe96fe5
Antonis Papatheodorou <antonisp@comino-systems.com>
2023-01-22 13:29:55 +0200

    structuring out application

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// App contains all the system depedencies like database connection, etc.
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up out APP")
+	return nil
+}
+
 func main() {
 	fmt.Print("hello")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up out REST API")
+		fmt.Println(err)
+	}
 }
